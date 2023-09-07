f4b447157ef8fefcbb115e54704298cd4061e118
BillySBarrett <billysb@stud.ntnu.no>
2023-09-08 00:06:47 +0200

    add wave to location cards

--- src/components/Card/CardSkeleton.tsx
+++ src/components/Card/CardSkeleton.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import WeatherIcon from "../atoms/icons/WeatherIcon";
 import './Card.css';
+import AnimatedWave from "../views/LocationCardsView/Wave";
 
 type CardProps = {
     weatherColor?: string;
@@ -14,7 +15,9 @@ const CardSkeleton: FC<CardProps> = (props) => {
     const { weatherColor, temperature, locationName, symbol_code, weatherDescription } = props;
 
     return (
-        <div className="card" style={{ backgroundColor: weatherColor || 'black' }}>
+        <div className="card" style={{
+            backgroundColor: weatherColor || 'black',
+        }}>
             <div className="card-content">
                 <div className="text-section">
                     <h3>
@@ -24,10 +27,13 @@ const CardSkeleton: FC<CardProps> = (props) => {
                         {locationName || "Loading..."}
                     </p>
                 </div>
-                <div className="wave-section">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 300" fill="none">
-                        <path d="M1440 169.816L1132.8 127.862C748.8 95.8964 748.8 159.827 384 191.793C57.6 223.758 88.5 169.816 0 159.827V0.000106812L1440 0.000106812V169.816Z" fill={weatherColor || 'black'}></path>
-                    </svg>
+                <div className="wave-section" style={{ backgroundColor: weatherColor || 'black' }}>
+                    <AnimatedWave
+                        color={"white"}
+                        //random number between 30 and 60
+                        animationDuration={`${Math.floor(Math.random() * (60 - 30 + 1)) + 30}s`}
+                        opacity={"1"}
+                    />
                     <div className="description-section">
                         {symbol_code ? <WeatherIcon symbol_code={symbol_code} /> :
                             //spinning loading icon
---
+++ src/components/views/LocationCardsView/Wave.tsx
@@ -0,0 +1,43 @@
+import { CSSProperties } from "react";
+import { renderToStaticMarkup } from "react-dom/server";
+
+type Props = {
+    color: CSSProperties["color"];
+    animationDuration: CSSProperties["animationDuration"];
+    animationDirection?: CSSProperties["animationDirection"];
+    opacity: CSSProperties["opacity"];
+};
+
+const AnimatedWave: React.FC<Props> = ({ color, ...props }) => {
+
+    const wave = (
+        <svg viewBox="0 0 1000 126" xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M0 42.9533C178.148 -12.5894 287.4 -13.7474 500 42.9533C682.727 88.1203 798.763 97.7368 1000 42.9533V125.861H0V42.9533Z"
+                fill={color}
+            />
+        </svg>
+    );
+
+    const svgAsString = renderToStaticMarkup(wave);
+    const encodedWaveSvg = encodeURIComponent(svgAsString);
+
+    return (
+        <div
+            style={{
+                background: `url('data:image/svg+xml;utf8,${encodedWaveSvg}')`,
+                position: "relative",
+                bottom: 0,
+                width: "100%",
+                height: `${Math.floor(Math.random() * (38 - 25 + 1)) + 25}px`,
+                backgroundSize: "300px 40px",
+                animation: `wave ${props.animationDuration} linear infinite`,
+                animationDirection: props.animationDirection,
+                opacity: props.opacity,
+            }}
+        ></div>
+    );
+};
+export default AnimatedWave;
+
+//this is a modified version of https://github.com/jmarioste/react-wave-effect
