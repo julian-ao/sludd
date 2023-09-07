83f69aedba2d0435752636b16016a7cd3d7e0f0a
Julian <julianao@stud.ntnu.no>
2023-09-07 13:10:47 +0200

    feat/style: Styled the location page
    Finished styling the location page. Made it responsive. Added a utlis file with some helper functions. Added a wind icon showing the wind direction.
    
    Issue: #7

--- src/components/Location-page/LocationComponent.tsx
+++ src/components/Location-page/LocationComponent.tsx
@@ -1,23 +1,22 @@
-
-import { FC } from 'react';
 import './location_page.css';
 import { useLocationWeatherQuery } from '../../lib/useLocationWeatherQuery';
+import { convertDateToReadable } from '../../lib/utils';
 
 type LocationComponentProps = {
     locationName: string;
 };
 
-const LocationComponent: FC<LocationComponentProps> = (props) => {
+const LocationComponent = (props : LocationComponentProps) => {
     const { locationName } = props;
 
     const { locationData, weatherQuery } = useLocationWeatherQuery(locationName);
 
     if (weatherQuery.isLoading || !locationData) {
-        return <div>Loading...</div>;
+        return <div className='location_main'>Loading...</div>;
     }
 
     if (weatherQuery.isError) {
-        return <div>Error loading data</div>;
+        return <div className='location_main'>Error loading data</div>;
     }
 
     // Retrieve current date and time
@@ -30,50 +29,70 @@ const LocationComponent: FC<LocationComponentProps> = (props) => {
     // Filter out the weather data for the current day
     const todaysWeather = weatherQuery.data.properties.timeseries.filter((timeSeriesItem) => {
         const date = new Date(timeSeriesItem.time);
-        return date.getHours() >= currentHour+1 && 
-               date.getDate() === currentDay && 
-               date.getMonth() === currentMonth && 
+        return date.getHours() >= currentHour &&
+               date.getDate() === currentDay &&
+               date.getMonth() === currentMonth &&
                date.getFullYear() === currentYear;
     });
 
-
     return (
-        <div className="location_main">
-            <div className="location_header">
-                <div className='location_header_name'>
-                    <img src="../src/assets/heart.svg" alt='Hjerte'/>
-                    <h1>{locationData.navn[0].stedsnavn[0].skrivemåte}</h1>
+        <div className='location_main'>
+            <div className='location_content'>
+                Last updated {convertDateToReadable(weatherQuery.data.properties.meta.updated_at)}<br/><br/>
+                <div className='location_header'>
+                    <div className='location_header_name'>
+                        <img src='../src/assets/heart.svg' alt='Hjerte'/>
+                        <h1>{locationData.navn[0].stedsnavn[0].skrivemåte}</h1>
+                    </div>
+                    <div className='location_header_info'>
+                        <h1>{weatherQuery.data.properties.timeseries[2].data.instant.details.air_temperature}°C</h1>
+                        <h1>{weatherQuery.data.properties.timeseries[1].data.next_1_hours.summary.symbol_code}</h1>
+                    </div>
                 </div>
-                <div className='location_header_info'>
-                    <h1>{weatherQuery.data.properties.timeseries[2].data.instant.details.air_temperature}°C</h1>
-                    <h1>{weatherQuery.data.properties.timeseries[1].data.next_1_hours.summary.symbol_code}</h1>
-                </div>
-            </div>
-            <div className='location_body'>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Tid</th>
-                            <th>Vær</th>
-                            <th>Temp.</th>
-                            <th>Nedbør</th>
-                            <th>Vind(kast) m/s</th>
-                            <th>Luftfuktighet</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {todaysWeather.map((weather, index) => (
-                            <tr key={index}>
-                                <td>{new Date(weather.time).getHours()}:00</td>
-                                <td>{weather.data.next_1_hours?.summary.symbol_code || 'N/A'}</td>
-                                <td>{weather.data.instant.details.air_temperature || 'N/A'}°C</td>
-                                <td>{weather.data.next_1_hours?.details.precipitation_amount } mm</td>
-                                <td>{weather.data.instant.details.wind_speed || 'N/A'} m/s</td>
-                                <td>{weather.data.instant.details.relative_humidity || 'N/A'}%</td>
+                <div className='location_body'>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Time</th>
+                                <th>Weather</th>
+                                <th>Temp.</th>
+                                <th>Rainfall</th>
+                                <th className='hidden_column'>Wind</th>
+                                <th className='hidden_column'>Air humidity</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {todaysWeather.map((weather, index) => (
+                                <tr key={index}>
+                                    <td>
+                                        {new Date(weather.time).getHours() === currentHour ? 'Now' : new Date(weather.time).getHours() + ':00'}
+                                    </td>
+                                    <td>{weather.data.next_1_hours?.summary.symbol_code || 'N/A'}</td>
+
+                                    <td style={{
+                                        color: weather.data.instant.details.air_temperature > 0 ? '#F58C8C' : 'blue',
+                                    }}>
+                                    {weather.data.instant.details.air_temperature || 'N/A'}°C
+                                    </td>
+
+                                    <td>{weather.data.next_1_hours?.details.precipitation_amount } mm</td>
+                                    <td className='hidden_column'>
+                                        {weather.data.instant.details.wind_speed || 'N/A'} m/s
+                                        <svg
+                                            className='wind_icon'
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 384 512"
+                                            style={{ transform: `rotate(${weather.data.instant.details.wind_from_direction}deg)` }}
+                                        >
+                                            <path fill='#6D6D6D' d="M169.4 502.6c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 402.7 224 32c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 370.7L86.6 329.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128z"/>
+                                        </svg>
+                                    </td>
+                                    <td className='hidden_column'>{weather.data.instant.details.relative_humidity || 'N/A'}%</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
---
+++ src/lib/utils.ts
@@ -0,0 +1,29 @@
+export const convertDateToReadable = (date: string) => {
+    const month = date.slice(5, 7);
+    const day = parseInt(date.slice(8, 10), 10).toString(); // Remove leading zero
+    const hour = date.slice(11, 13);
+    const minute = date.slice(14, 16);
+
+    const daySuffix = getDaySuffix(Number(day)); // Get the day suffix
+
+    return `${day}${daySuffix} ${months[Number(month) - 1].toLocaleLowerCase()} ${hour}:${minute}`;
+};
+
+const getDaySuffix = (day: number) => {
+    if (day >= 11 && day <= 13) {
+      return 'th';
+    }
+    const lastDigit = day % 10;
+    switch (lastDigit) {
+      case 1:
+        return 'st';
+      case 2:
+        return 'nd';
+      case 3:
+        return 'rd';
+      default:
+        return 'th';
+    }
+};
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November','December'];
