62f9de05535e93b24009049ee50e22f4a34434b6
Julian <julianao@stud.ntnu.no>
2023-09-09 14:17:31 +0200

    feat:document title and favicon
    added favicon and document title based on the slug
    
    Closes: #18

--- src/pages/slug.tsx
+++ src/pages/slug.tsx
@@ -1,9 +1,19 @@
-// BlogPost.js
 import { useParams } from 'react-router-dom';
 import LocationComponent from '../components/Location-page/LocationComponent';
+import { useEffect } from 'react';
 
 const LocationPage = () => {
     const { slug } = useParams();
+
+    useEffect(() => {
+        // Set the document title to the slug when the component mounts
+        document.title = 'Sludd - ' + slug || 'Sludd';
+
+        // Optionally, you can return a cleanup function if needed
+        return () => {
+            document.title = 'Sludd'; // Reset the title when the component unmounts
+        };
+    }, [slug]);
 
     return (
         <LocationComponent locationName={slug || ''}  />
