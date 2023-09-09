f03a4b058cb6449ef27d4df80c062d403c0acaa1
Julian <julianao@stud.ntnu.no>
2023-09-09 14:19:03 +0200

    chore: comment
    removed comment
    
    Issue: #18

--- src/pages/slug.tsx
+++ src/pages/slug.tsx
@@ -9,7 +9,6 @@ const LocationPage = () => {
         // Set the document title to the slug when the component mounts
         document.title = 'Sludd - ' + slug || 'Sludd';
 
-        // Optionally, you can return a cleanup function if needed
         return () => {
             document.title = 'Sludd'; // Reset the title when the component unmounts
         };
